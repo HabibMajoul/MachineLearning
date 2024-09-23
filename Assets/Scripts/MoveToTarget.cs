c98d351f0e4ad52cbf60ea9eb35a4d79f0a38726
HabibMajoul <habib.majoul@esprit.tn>
2024-09-23 23:04:05 +0100

    .

--- Assets/Scripts/MoveToTarget.cs
+++ Assets/Scripts/MoveToTarget.cs
@@ -22,6 +22,7 @@ public class MoveToTarget : Agent
     [SerializeField] private List<Transform> obstaclePositions1 = new List<Transform>();
     [SerializeField] private List<Transform> obstaclePositions2 = new List<Transform>();
     [SerializeField] private List<Transform> obstaclePositions3 = new List<Transform>();
+    private float distanceRewardMultiplier = 1.0f;
 
     public void RandomizePosition(List<Transform> list, int floor){
         
@@ -42,10 +43,9 @@ public class MoveToTarget : Agent
     public override void OnEpisodeBegin()
     {
         characterAnimationManager.OnResetEpisode();
+        distanceRewardMultiplier = 1.0f;
 
-        StartCoroutine(Wait(0.5f));
-
-        yLevel = transform.localPosition.y;
+        yLevel = -0.1854539f;
 
         foreach (Transform obj in checkpoints){
             obj.gameObject.SetActive(true);
@@ -57,10 +57,6 @@ public class MoveToTarget : Agent
         RandomizePosition(obstaclePositions1, 1);
         RandomizePosition(obstaclePositions2, 2);
         RandomizePosition(obstaclePositions3, 3);
-    }
-
-    IEnumerator Wait(float delay){
-        yield return new WaitForSeconds(delay);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -85,19 +81,26 @@ public class MoveToTarget : Agent
         moveX = actions.ContinuousActions[0];
         moveZ = actions.ContinuousActions[1];
         
-        AddReward(-0.000001f);
+        AddReward(-0.00000125f * distanceRewardMultiplier);
 
         //Adds reward the closer they get to checkpoint
         if (distanceToCheckpoint > Vector3.Distance(transform.localPosition, nextCheckpoint.localPosition)){
-            AddReward(0.00000125f);
+            AddReward(0.000005f * distanceRewardMultiplier);
         }
         else
         {
-            AddReward(-0.000002f);
+            AddReward(-0.00000125f * distanceRewardMultiplier);
         }
         distanceToCheckpoint = Vector3.Distance(transform.localPosition, nextCheckpoint.localPosition);
 
-        if (yLevel > transform.localPosition.y) AddReward(-0.1f); 
+        if (yLevel > transform.localPosition.y)
+        {
+            AddReward(-0.2f);
+        }
+        if (yLevel < transform.localPosition.y)
+        {
+            AddReward(0.09f);
+        }
         yLevel = transform.localPosition.y;
     }
 
@@ -113,17 +116,24 @@ public class MoveToTarget : Agent
         if (other.gameObject.CompareTag("Reward"))
         {
             other.gameObject.SetActive(false);
-            AddReward(20f);
+            AddReward(1f + (10f * distanceRewardMultiplier));
             nextCheckpoint = checkpoints[++checkpoint];
-            Debug.Log("Checkpoint reached!");
+            distanceRewardMultiplier *= 2;
+            Debug.Log("Checkpoint " + (checkpoint - 1) + " reached!");
         }
         if (other.gameObject.CompareTag("FinalReward"))
         {
             other.gameObject.SetActive(false);
-            AddReward(50f);
+            AddReward(50f * distanceRewardMultiplier);
             Debug.Log("Final checkpoint reached!");
         }
-        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("obstacle"))
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-20f);
+            characterAnimationManager.characterController.enabled = false;
+            EndEpisode();
+        }
+        if (other.gameObject.CompareTag("obstacle"))
         {
             AddReward(-10f);
             characterAnimationManager.characterController.enabled = false;
@@ -131,7 +141,7 @@ public class MoveToTarget : Agent
         }
         if (other.gameObject.CompareTag("Wall2"))
         {
-            AddReward(-15);
+            AddReward(-70);
             characterAnimationManager.characterController.enabled = false;
             EndEpisode();
         }
